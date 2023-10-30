3fa3a535436e04b50d895b4131287c089694a00f
Alistair Chambers <alicham6@gmail.com>
2023-10-30 18:43:54 -0500

    converted to tabs

---
+++ script.js
@@ -0,0 +1,9 @@
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => (tab.style.display = 'none'));
+
+    const tabToShow = document.getElementById(tabId);
+    if (tabToShow) {
+        tabToShow.style.display = 'block';
+    }
+}
